a4ae4f338312020d5709a533e0a2016be983d91d
newton224 <newtonandala224@gmail.com>
2026-02-07 09:30:58 +0300

    Update bot settings and owner information

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'MiaKhalifa AI',
+  author: '‎AndalaTheMr',
+  botName: "MiaKhalifa",
+  botOwner: 'MiaKhalifa', // Your name
+  ownerNumber: '254793753327', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
